04bcf51ec89e710f94a7270c78cfda663b724d74
Peter Vliet <petervliet@Peters-MacBook-Air.local>
2015-02-17 14:23:49 -0700

    only logged in users can see menu options

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -8,7 +8,7 @@ class ClubsController < ApplicationController
   def show
     @club = Club.find(params[:id])
       if !@club.students.find_by_id(current_student.id)
-        render 'public/403.html'
+        render 'public/403', status: 403
       end
   end
 
